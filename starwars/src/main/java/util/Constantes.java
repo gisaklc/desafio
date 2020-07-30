fd80323b6f972107f3f8b2afd1c07f2542f57840
gisaklc <gisaklc@hotmail.com>
2020-07-30 17:13:05 -0300

    Acerto no tipo do campo da classe

--- starwars/src/main/java/util/Constantes.java
+++ starwars/src/main/java/util/Constantes.java
@@ -5,4 +5,6 @@ public class Constantes {
 	public static final String NAO_ENCONTRADO = "Planeta não encontrado";
 	public static final String PLANETA_JA_CADASTRADO = "Planeta já cadastrado";
 	public static final String PLANETA_DELETADO = "Planeta deletado!";
+	public static final int INCLUIR = 1;
+	public static final int ALTERAR = 2;
 }
